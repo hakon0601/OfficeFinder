047ed2de5105429a89288e03abb2e53e39761474
Håkon Gimse <hakongimse@gmail.com>
2021-11-06 13:18:21 +0100

    compose

--- app/src/main/java/com/example/officefinder/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/officefinder/ui/profile/ProfileFragment.kt
@@ -5,20 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.officefinder.databinding.FragmentProfileBinding
+import com.example.officefinder.R
+//import com.example.officefinder.databinding.FragmentProfileBinding
 import com.example.officefinder.ui.profile.ProfileViewModel
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
-    private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,18 +36,35 @@ class ProfileFragment : Fragment() {
         profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_profile, container, false).apply {
+            findViewById<ComposeView>(R.id.compose_view).setContent {
+                Greeting()
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
+}
+
+@Composable
+private fun Greeting() {
+    Text(
+        text = "Velkommen til din profil",
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+
+    Text(
+        text = "Håkon",
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(R.dimen.margin_small), vertical= dimensionResource(R.dimen.margin_small)*4)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
